8ae2724bbe0828a7874d04217d24c9d1fa13efb8
Dmitry Zakharov <dzakh.dev@gmail.com>
2024-06-21 19:26:13 +0400

    Adjust indexer

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -1,33 +1,34 @@
-/*
- * Please refer to https://docs.envio.dev for a thorough guide on all Envio indexer features
- */
-import {
-  SwayFarmContract,
-  SwayFarm_SellItemEntity,
-  SwayFarm_LevelUpEntity,
-  SwayFarm_NewPlayerEntity,
-} from "generated";
+import { SwayFarmContract } from "generated";
 
-SwayFarmContract.SellItem.handler(({ event, context }) => {
-  const entity: SwayFarm_SellItemEntity = {
-    id: `${event.transactionId}_${event.receiptIndex}`,
-  };
-
-  context.SwayFarm_SellItem.set(entity);
+/**
+Registers a handler that processes NewPlayer event
+on the SwayFarm contract and stores the players in the DB
+*/
+SwayFarmContract.NewPlayer.handler(({ event, context }) => {
+  // Set the Player entity in the DB with the intial values
+  context.Player.set({
+    // The address in Sway is a union type of user Address and ContractID. Envio supports most of the Sway types, and the address value was decoded as a discriminated union 100% typesafe
+    id: event.data.address.payload.bits,
+    // Initial values taken from the contract logic
+    farmingSkill: 1n,
+    totalValueSold: 0n,
+  });
 });
 
 SwayFarmContract.LevelUp.handler(({ event, context }) => {
-  const entity: SwayFarm_LevelUpEntity = {
-    id: `${event.transactionId}_${event.receiptIndex}`,
-  };
-
-  context.SwayFarm_LevelUp.set(entity);
+  const playerInfo = event.data.player_info;
+  context.Player.set({
+    id: event.data.address.payload.bits,
+    farmingSkill: playerInfo.farming_skill,
+    totalValueSold: playerInfo.total_value_sold,
+  });
 });
 
-SwayFarmContract.NewPlayer.handler(({ event, context }) => {
-  const entity: SwayFarm_NewPlayerEntity = {
-    id: `${event.transactionId}_${event.receiptIndex}`,
-  };
-
-  context.SwayFarm_NewPlayer.set(entity);
+SwayFarmContract.SellItem.handler(({ event, context }) => {
+  const playerInfo = event.data.player_info;
+  context.Player.set({
+    id: event.data.address.payload.bits,
+    farmingSkill: playerInfo.farming_skill,
+    totalValueSold: playerInfo.total_value_sold,
+  });
 });
